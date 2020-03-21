b7fad25a7567baf42a65803a2f4b79cacd23c901
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-03-21 00:16:29 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('dotenv').config
 const server = require("./api/server.js");
 
-const port = 5000;
+// make the port be assigned by the server
+// heroku will place the .PORT environment value on their server
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// CI/CD pipeline stages:
+// * development (dev)
+// * test
+// * staging
+// * --- manual gate
+// * production
