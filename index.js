495d827ddd68dd4a41342c058c7760319f6871e3
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-03-21 00:58:58 -0400

    motd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config
+require('dotenv').config() // read from a .env file located at the root folder of the project and merge the key/value pairs into process.env
 const server = require("./api/server.js");
 
 // make the port be assigned by the server
